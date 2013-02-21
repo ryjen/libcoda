ac4f55603c4ff50ab87a03e3ee6a6a66d5039cb1
c0der78 <c0der78@gmail.com>
2013-02-21 00:18:19 -0800

    updated db iterators (avoid public domain code used to quickly test)

--- premake4.lua
+++ premake4.lua
@@ -6,6 +6,48 @@ newaction {
       os.execute("astyle -N -A1 -R '*.cpp' '*.h'");
    end
 }
+newaction {
+    trigger     = "install",
+    description = "Install headers and lib",
+    execute = function() 
+
+      if not _ARGS[1] or not os.isdir(_ARGS[1]) then
+        error("You must specify an install location")
+      end
+
+      if not os.isdir("bin/Release") then
+        error("You must make a release build first")
+      end
+
+      bindir = _ARGS[1].."/lib"
+      headerdir = _ARGS[1].."/include/arg3"
+
+      if not os.isdir(bindir) then
+        os.mkdir(bindir)
+      end
+
+      if not os.isdir(headerdir) then
+        os.mkdir(headerdir)
+      end
+
+      libraries = { "libarg3.a", "libarg3db.a", "libarg3dice.a", "libarg3format.a", "libarg3strings.a"}
+      for l=1, #libraries do
+        if os.isfile("bin/Release/"..libraries[l]) then
+            os.copyfile("bin/Release/"..libraries[l], bindir)
+        end
+      end
+
+      folders = {"collections", "db", "dice", "format", "strings", "variant"}
+      for f=1, #folders do 
+          headers = os.matchfiles(folders[f].."/**.h")
+          tempdir = headerdir.."/"..folders[f]
+          os.mkdir(tempdir)
+          for i=1, #headers do
+            os.copyfile(headers[i], tempdir)
+          end
+      end
+    end
+}
 
 newoption {
    trigger     = "monolithic",
@@ -17,15 +59,15 @@ if _ACTION == "clean" then
     for i=1, #matches do
         os.remove(matches[i])
     end
+    os.rmdir("bin")
+    os.rmdir("obj")
 end
 
 solution "arg3"
     configurations { "Debug", "Release" }
     kind "StaticLib"
     language "C++"
-    configuration "Debug"
-        flags "Symbols"
-    
+
     buildoptions { "-std=c++11", "-stdlib=libc++", "-Wall", "-Werror"}
 
     linkoptions { "-stdlib=libc++" }
@@ -49,6 +91,12 @@ solution "arg3"
                 "**.test.cpp"
             }
     end
+    
+    configuration "Debug"
+        flags "Symbols"
+        targetdir "bin/Debug"
+    configuration "Release"
+        targetdir "bin/Release"
     
     project "arg3test"
         kind "ConsoleApp"
--- strings/strings.test.cpp
+++ strings/strings.test.cpp
@@ -8,10 +8,52 @@ using namespace arg3;
 
 Context(arg3strings)
 {
-    Spec(is_valid_email_test)
-    {
-        string email = "somedude@email.com.ca";
 
-        Assert::That(is_valid_email(email), Equals(true));
+    Spec(isNumber)
+    {
+        Assert::That(is_number("45"), Equals(1));
+        Assert::That(is_number("4.5"), Equals(2));
+        Assert::That(is_number("-4.5"), Equals(2));
+        Assert::That(is_number("-45"), Equals(1));
+
+        Assert::That(is_number("asdf"), Equals(0));
     }
+
+    Spec(isValidEmail)
+    {
+        Assert::That(is_valid_email("c0der78@gmail.com"), Equals(true));
+        Assert::That(is_valid_email("ryan.jennings@arg3.net"), Equals(true));
+        Assert::That(is_valid_email("ryan@blah"), Equals(false));
+        Assert::That(is_valid_email("@blah.com"), Equals(false));
+        Assert::That(is_valid_email("ryan@"), Equals(false));
+        Assert::That(is_valid_email("ryan.jennings.net"), Equals(false));
+    }
+
+    Spec(ordinalString)
+    {
+        Assert::That(ordinal_string(1), Equals("first"));
+        Assert::That(ordinal_string(2), Equals("second"));
+        Assert::That(ordinal_string(3), Equals("third"));
+        Assert::That(ordinal_string(4), Equals("4th"));
+        Assert::That(ordinal_string(17), Equals("17th"));
+        Assert::That(ordinal_string(21), Equals("21st"));
+        Assert::That(ordinal_string(22), Equals("22nd"));
+        Assert::That(ordinal_string(23), Equals("23rd"));
+    }
+
+    
+    Spec(testCapitalize)
+    {
+
+        Assert::That(capitalize("fooBAR"), Equals("Foobar"));
+    }
+
+
+    Spec(testNullstr)
+    {
+        Assert::That(nullstr(""), Equals(true));
+        Assert::That(nullstr(0), Equals(true));
+        Assert::That(nullstr("test"), Equals(false));
+    }
+
 };
