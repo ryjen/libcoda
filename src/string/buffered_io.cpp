ccbd68434517dedbb6944b035bb42f03abf1d650
Ryan Jennings <c0der78@gmail.com>
2014-02-07 17:11:16 -0800

    I would rather be playing SC2.

---
+++ src/string/buffered_io.cpp
@@ -0,0 +1,88 @@
+#include "buffered_io.h"
+
+using namespace std;
+
+namespace arg3
+{
+    string buffered_reader::readln()
+    {
+        if (buffer_.empty()) return string();
+
+        /* find a new line character */
+        auto pos = find_first_of(buffer_.begin(), buffer_.end(), NEWLINE.begin(), NEWLINE.end());
+
+        if (pos == buffer_.end())
+        {
+            string temp(buffer_.begin(), buffer_.end());
+
+            buffer_.clear();
+
+            return temp;
+        }
+
+        string temp(buffer_.begin(), pos);
+
+        /* Skip all new line characters */
+        while (pos != buffer_.end() && find(NEWLINE.begin(), NEWLINE.end(), *pos) != NEWLINE.end())
+        {
+            pos++;
+        }
+
+        buffer_.erase(buffer_.begin(), pos);
+
+        return temp;
+    }
+
+    bool buffered_reader::has_input() const
+    {
+        return !buffer_.empty();
+    }
+
+    const buffered_data &buffered_reader::input() const
+    {
+        return buffer_;
+    }
+
+    bool buffered_writer::has_output() const
+    {
+        return !buffer_.empty();
+    }
+
+    const buffered_data &buffered_writer::output() const
+    {
+        return buffer_;
+    }
+
+    buffered_writer &buffered_writer::writeln(const string &value)
+    {
+        buffer_.insert(buffer_.end(), value.begin(), value.end());
+        buffer_.insert(buffer_.end(), NEWLINE.begin(), NEWLINE.end());
+        return *this;
+    }
+
+    buffered_writer &buffered_writer::writeln(const buffered_data &value)
+    {
+        buffer_.insert(buffer_.end(), value.begin(), value.end());
+        buffer_.insert(buffer_.end(), NEWLINE.begin(), NEWLINE.end());
+        return *this;
+    }
+
+    buffered_writer &buffered_writer::writeln()
+    {
+        buffer_.insert(buffer_.end(), NEWLINE.begin(), NEWLINE.end());
+        return *this;
+    }
+
+    buffered_writer &buffered_writer::write(const string &value)
+    {
+        buffer_.insert(buffer_.end(), value.begin(), value.end());
+        return *this;
+    }
+
+
+    buffered_writer &buffered_writer::write(const buffered_data &value)
+    {
+        buffer_.insert(buffer_.end(), value.begin(), value.end());
+        return *this;
+    }
+}
---
+++ src/string/buffered_io.h
@@ -0,0 +1,88 @@
+#ifndef ARG3_STRING_BUFFERED_IO_H_
+#define ARG3_STRING_BUFFERED_IO_H_
+
+#include <vector>
+#include <string>
+
+namespace arg3
+{
+
+    typedef std::vector<unsigned char> buffered_data;
+
+    static const buffered_data NEWLINE = { '\r', '\n' };
+
+    class buffered_reader
+    {
+    public:
+        /*!
+         * Reads a line from the read buffer
+         */
+        std::string readln();
+
+        /*!
+         * @returns the read buffer
+         */
+        const buffered_data &input() const;
+
+        buffered_data &input();
+
+        /*!
+         * @returns true if the read buffer contains data
+         */
+        bool has_input() const;
+
+        /*!
+         * Reads data into the read buffer
+         * @returns true if no errors occured
+         */
+        virtual bool read_to_buffer() = 0;
+    private:
+        buffered_data buffer_;
+    };
+
+    class buffered_writer
+    {
+    public:
+
+        /*!
+         * Appends some data to the write buffer with a new line.
+         */
+        buffered_writer &writeln(const std::string &value);
+
+        buffered_writer &writeln(const buffered_data &value);
+
+        /*!
+        * Appends a new line to the write buffer
+        */
+        buffered_writer &writeln();
+
+        /*!
+         * Appends some data to the write buffer.
+         */
+        buffered_writer &write(const std::string &value);
+
+        buffered_writer &write(const buffered_data &value);
+
+        /*!
+         * @returns the write buffer
+        */
+        const buffered_data &output() const;
+
+        buffered_data &output();
+
+        /*!
+         * @returns true if the write buffer contains data
+         */
+        bool has_output() const;
+
+        /*!
+         * Will write the buffer
+         * @returns true if no errors occured
+         */
+        virtual bool write_from_buffer() = 0;
+    private:
+        buffered_data buffer_;
+    };
+}
+
+#endif
