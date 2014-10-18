1c3eefb2a0e9b404f0c56b2cd691c1ed16ebaab5
Ryan Jennings <c0der78@gmail.com>
2014-10-18 11:04:33 -0700

    working with homebrew updates

--- homebrew/arg3.rb
+++ homebrew/arg3.rb
@@ -5,8 +5,12 @@ class Arg3 < Formula
   url 'https://github.com/c0der78/libarg3.git'
   sha1 '6177abcc886d9d16190d2ce449d1c18897c56d04'
 
+  head 'http://github.com/c0der78/libarg3.git'
+	
+  version '0.5'
+	
   devel do
-    url 'file:///Users/c0der78/Devel/cpp/arg3/arg3-0.5.tar.gz'
+    url 'file:///Users/Ryan/Devel/libarg3/arg3-0.5.tar.gz'
   end
 
   def install
