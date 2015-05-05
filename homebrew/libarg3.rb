3f5546304254c10963633593f2efacc3d994974e
Ryan Jennings <c0der78@gmail.com>
2015-05-04 23:28:01 -0700

    just shoot me

--- homebrew/libarg3.rb
+++ homebrew/libarg3.rb
@@ -2,16 +2,12 @@ require 'formula'
 
 class Libarg3 < Formula
   homepage 'http://github.com/c0der78/libarg3'
-  url 'https://github.com/c0der78/libarg3.git'
+  url 'https://github.com/c0der78/libarg3/archive/0.5.1.tar.gz'
   sha1 '6177abcc886d9d16190d2ce449d1c18897c56d04'
 
   head 'http://github.com/c0der78/libarg3.git'
 	
-  version '0.1'
-	
-  devel do
-    url 'file:///Users/Ryan/Devel/libarg3/arg3-0.1.tar.gz'
-  end
+  version '0.5.1'
 
   depends_on 'json-c'
   depends_on 'mysql'
