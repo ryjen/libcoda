5304368e62e4c5e60a388fa14e742f0b773fe6ca
Ryan Jennings <c0der78@gmail.com>
2015-05-05 22:33:19 -0700

    bifurcation

--- homebrew/libarg3.rb
+++ homebrew/libarg3.rb
@@ -3,12 +3,14 @@ require 'formula'
 class Libarg3 < Formula
   homepage 'http://github.com/c0der78/libarg3'
   url 'https://github.com/c0der78/libarg3/archive/0.5.1.tar.gz'
-  sha1 '2cc85dfcc6f074d79ec33fc11d41a5bbcf34234b'
+  sha1 '168aa6a1d06119d2e64b5bdfec82799c411dcfb5'
 
   head 'http://github.com/c0der78/libarg3.git'
 
   version '0.5.1'
 
+  depends_on 'automake'
+  depends_on 'autoconf'
   depends_on 'json-c'
   depends_on 'mysql'
   depends_on 'sqlite3'
@@ -17,7 +19,7 @@ class Libarg3 < Formula
 
   def install
     ENV.universal_binary
-    system "./bootstrap.sh", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     ENV.deparallelize
     system "make"
     system "make install"
--- src/arg3.cpp
+++ src/arg3.cpp
@@ -1,8 +1,9 @@
 #include "config.h"
 #include <stdio.h>
 
-char* arg3_version() {
-	static char buf[BUFSIZ+1] = {0};
-  snprintf(buf, BUFSIZ, "%s", VERSION);
+char* arg3_version()
+{
+	static char buf[BUFSIZ + 1] = {0};
+	snprintf(buf, BUFSIZ, "%s", VERSION);
 	return buf;
 }
